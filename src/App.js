d1204647a55c035fcc33b2b31fc59e8deb764965
Gagan <vishwakarmagagan24@gmail.com>
2024-05-12 09:44:17 +0530

    modified:   .gitignore 	modified:   .netlify/state.json 	deleted:    public/first.jpg 	modified:   src/App.js 	modified:   src/components/HotAccessories.js 	modified:   src/components/NavOptios.js 	modified:   src/components/Navbar.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
 import {  BrowserRouter as Router,Route } from "react-router-dom"
 import Slider from "./components/Slider.js"
-import data from "./data/data.json"
+import data from "./data/data.json";
 import Offers from "./components/Offers.js"
 import Heading  from "./components/Heading.js"
 import StarProduct from "./components/StarProduct.js"
--- src/components/NavOptios.js
+++ src/components/NavOptios.js
@@ -1,104 +1,98 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import NavCard from "./NavCard.js"
 import "../styles/NavOptios.css"
+import { useLocation } from 'react-router-dom';
+const NavOptios = ({ miPhones, redmiPhones, tv, laptop, fitnessAndLifeStyle, home, audio, accessories }) => {
 
+    const [miPhoneToggle, setMiPhoneToggle] = useState(false);
+    const [redmiPhoneToggle, setRedmiPhoneToggle] = useState(false);
+    const [tvToggle, setTvToggle] = useState(false);
+    const [laptopToggle, setLaptopToggle] = useState(false);
+    const [fitnessToggle, setFitnessToggle] = useState(false);
+    const [homeToggle, setHomeToggle] = useState(false);
+    const [audioToggle, setAudioToggle] = useState(false);
+    const [accessoriesToggle, setAccessoriesToggle] = useState(false);
+    let location = useLocation();
 
+    useEffect(() => {
 
+        if (location.pathname === "/miphones") {
+            return setMiPhoneToggle(true)
+        }
+        if (location.pathname === "/redmiphones") {
+            return setRedmiPhoneToggle(true)
+        }
+        if (location.pathname === "/tv") {
+            return setTvToggle(true)
+        }
+        if (location.pathname === "/laptops") {
+            return setLaptopToggle(true)
+        }
+        if (location.pathname === "/lifestyle") {
+            return setFitnessToggle(true)
+        }
+        if (location.pathname === "/home") {
+            return setHomeToggle(true)
+        }
+        if (location.pathname === "/audio") {
+            return setAudioToggle(true)
+        }
 
-const NavOptios = ( {miPhones,redmiPhones,tv,laptop,fitnessAndLifeStyle,home,audio,accessories}) => {
-
-   const [miPhoneToggle, setMiPhoneToggle] = useState(false);
-   const [redmiPhoneToggle,setRedmiPhoneToggle] = useState(false);
-   const [tvToggle,setTvToggle] = useState(false);
-   const [laptopToggle,setLaptopToggle] = useState(false);
-   const [fitnessToggle,setFitnessToggle] = useState(false);
-   const [homeToggle,setHomeToggle] = useState(false);
-   const [audioToggle,setAudioToggle] = useState(false);
-   const [accessoriesToggle,setAccessoriesToggle] = useState(false);
- 
-
-   useEffect(() => {
-     
-    if(window.location.pathname === "/#miphones"){
-        return   setMiPhoneToggle(true)
-       }
-     if(window.location.pathname === "/#redmiphones"){
-        return   setRedmiPhoneToggle(true)
-       }
-       if(window.location.pathname === "/#tv"){
-        return   setTvToggle(true)
-       }
-       if(window.location.pathname === "/#laptops"){
-        return   setLaptopToggle(true)
-       }
-       if(window.location.pathname === "/#lifestyle"){
-        return  setFitnessToggle(true)
-       }
-       if(window.location.pathname === "/#home"){
-        return  setHomeToggle(true)
-       }
-       if(window.location.pathname === "/#audio"){
-        return  setAudioToggle(true)
-       }
-       
-       if(window.location.pathname === "/#accessories"){
-        return  setAccessoriesToggle(true)
-       }
-
-       
-   }, [])
+        if (location.pathname === "/accessories") {
+            return setAccessoriesToggle(true)
+        }
+         }, [])
 
     return (
-        <div className="navOptions"> 
+        <div className="navOptions">
 
-            {miPhoneToggle?   miPhones.map((item)=>(
+            {miPhoneToggle ? miPhones.map((item) => (
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
 
-                 < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-              )) : null }
-            
-      
-            {redmiPhoneToggle?   redmiPhones.map((item)=>(
 
-               < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-           )) : null }
-     
-
-            {tvToggle ?   tv.map((item)=>(
+            {redmiPhoneToggle ? redmiPhones.map((item) => (
 
                 < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-           )) : null }
+            )) : null}
 
-           {laptopToggle ?   laptop.map((item)=>(
 
-              < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-           )) : null }
-     
-   
-
-            {fitnessToggle ?   fitnessAndLifeStyle.map((item)=>(
-
-              < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-           )) : null }
-     
-
-               {homeToggle ?   home.map((item)=>(
-
-                   < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-              )) : null }
-     
-     
-
-            {audioToggle ?   audio.map((item)=>(
-
-             < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-            )) : null }
-     
-
-            {accessoriesToggle ?   accessories.map((item)=>(
+            {tvToggle ? tv.map((item) => (
 
                 < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
-             )) : null }
-     
+            )) : null}
+
+            {laptopToggle ? laptop.map((item) => (
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
+
+
+
+            {fitnessToggle ? fitnessAndLifeStyle.map((item) => (
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
+
+
+            {homeToggle ? home.map((item) => (
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
+
+
+
+            {audioToggle ? audio.map((item) => (
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
+
+
+            {accessoriesToggle ? accessories.map((item) => (
+
+                < NavCard name={item.name} price={item.price} image={item.image} key={item.image} />
+            )) : null}
+
         </div>
     )
 }
